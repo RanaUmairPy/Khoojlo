c0bf4013237f312700e5c7769333b665f080ff76
rumairpy <umairsaeed320@gmail.com>
2026-01-17 14:24:31 +0500

    seednode setup

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: ['aefbb21f348f.ngrok-free.app','web-8rnicojgsprk.up-de-fra1-k8s-1.apps.run-on-seenode.com'] // Replace with your actual Ngrok subdomain
+  preview: {
+    host: '0.0.0.0',
+    port: 80,
+    allowedHosts: [
+      'aefbb21f348f.ngrok-free.app',
+      'web-8rnicojgsprk.up-de-fra1-k8s-1.apps.run-on-seenode.com'
+    ]
   }
 })
